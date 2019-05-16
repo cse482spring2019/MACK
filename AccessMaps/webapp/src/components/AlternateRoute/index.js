b86e5dab6f7421b8589aa10a89fc3ce01a006280
Ken Dong <kedong@cs.washington.edu>
2019-05-16 14:55:14 -0700

    alternateRoute fixes

--- AccessMaps/webapp/src/components/AlternateRoute/index.js
+++ AccessMaps/webapp/src/components/AlternateRoute/index.js
@@ -5,19 +5,23 @@ import Button from "react-md/src/js/Buttons";
 import { CardText } from "react-md/src/js/Cards";
 import Toolbar from "react-md/src/js/Toolbars";
 
+import { routeResult as routeResultProps } from "prop-schema";
+
 const AlternateRoute = props => {
   const { onClose, routeResult } = props;
+
+  const route = routeResult.routes[0];
   return (
     <React.Fragment>
       <Toolbar
         title="Find an Alternate Route"
         actions={[
-          <Button key="close-alternatte-route-button" icon onClick={onClose}>
+          <Button key="close-trip-info-button" icon onClick={onClose}>
             close
           </Button>
         ]}
       />
-      <CardText className="alternate-route--contents">
+      <CardText className="route-info--contents">
         <h4>Please mark the location of the obstruction on the route</h4>
       </CardText>
     </React.Fragment>
@@ -34,4 +38,4 @@ Directions.defaultProps = {
   routeResult: null
 };
 
-export default RouteInfo;
+export default AlternateRoute;
