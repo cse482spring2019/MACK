6dc7ceff47340e34dbd733e991c6a23917d480f1
kdong <kdong1775@gmail.com>
2019-05-28 12:42:37 -0700

    test

--- AccessMaps/accessmap/deploy_dev.sh
+++ AccessMaps/accessmap/deploy_dev.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.build.yml -f docker-compose.build.override.yml build
-docker-compose -f docker-compose.yml -f docker-compose.override.yml build	
+docker-compose -f docker-compose.build.yml -f docker-compose.build.override.yml up 
+#docker-compose -f docker-compose.yml -f docker-compose.override.yml build	
 docker-compose -f docker-compose.yml -f docker-compose.override.yml up
